8bd54470e29931a7d9f4165613b070a8429abfcf
officialnitish5657 <142983289+officialnitish5657@users.noreply.github.com>
2024-03-22 01:12:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+let speechtext = '';
+if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+    let speech = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    speech.leng = "en-US";
+    speech.onstart = function () {
+        console.log("start to speech..");
+
+    }
+    speech.onerror = function () {
+        console.log("error, let's try again!");
+    }
+
+    speech.onresult = function (e) {
+        console.log(e.results[0][0].transcript);
+        document.querySelector("#chats").innerHTML = e.results[0][0].transcript;
+        
+        // speechtext = e.results[0][0].transcript;
+
+    }
+
+    document.querySelector("#speech").addEventListener("click", function () {
+        speech.start();
+    });
+
+
+}
+else {
+    console.log("you brouser not support speech funcanality");
+}
+
+function speak() {
+    speechtext = document.querySelector("#chats").value;
+    let spk = new SpeechSynthesisUtterance();
+    spk.leng = "en-US";
+    if(speechtext===''){
+        text="plese speech..";
+    }
+    else{
+        text=speechtext;
+    }
+
+    spk.text = text;
+    spk.rate = 0.5;
+    window.speechSynthesis.speak(spk)
+
+}
+document.querySelector("#speak").addEventListener("click", function () {
+    speak();
+});
+
+
+function mic(){
+
+    let spee = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    spee.leng = "en-US";
+    spee.onstart = function () {
+        console.log("start to speech..");
+        
+    }
+    spee.onerror = function () {
+        console.log("error, let's try again!");
+    }
+    
+    spee.onresult = function (e) {
+        // console.log(e.results[0][0].transcript);
+        transcript = e.results[0][0].transcript;
+        document.querySelector("#google-search").value =transcript ;
+        
+    }  
+    return spee
+}
+    document.querySelector("#mic").addEventListener("click", function () {
+       let s=mic();
+        s.start();
+    });
+    // document.querySelector("#google-search").innerHTML ="hello"
+
+    function searchGoogle() {
+        var searchtext = document.getElementsByClassName("searchtext").value;
+        var searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(searchtext);
+        window.open(searchUrl, '_blank');
+      }
+
+      document.querySelector("#google-search").addEventListener("keypress",function(e){
+        if(e.key==='Enter')
+        searchGoogle();
+    else
+    console.log(e);
+      })
